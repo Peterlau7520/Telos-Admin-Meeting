36120e148828a809b1432cb0a2f006faa4e5e2e3
mishalinkites1 <misha@linkites.com>
2018-01-19 11:39:51 +0530

    open routes

--- routes/index.js
+++ routes/index.js
@@ -21,8 +21,6 @@ router.get('/404', (req, res) => {
   res.render('error', {layout: 'errorLayout.hbs'});
 });
 
-/*router.get('/siteMap', (req, res) => {
-
 router.get('/privacy', (req, res) => {
   res.render('privacy', {layout: 'privacyLayout.hbs'});
 });
@@ -40,23 +38,23 @@ router.get('/termschn', (req, res) => {
   res.render('termschn', {layout: 'termsLayout.hbs'});
 });
 
-/**/
-/*router.get('/siteMap', (req, res) => {
+
+router.get('/siteMap', (req, res) => {
   res.render('sitemap', {layout: 'sitemap.hbs'});
 });
-*/
+
 router.get('/sitemap.xml', (req, res) => {
   res.type('text/plain');
   res.render('sitemap', {layout: 'sitemap.xml'});
 });
 
-/*router.get('/robots.txt', function (req, res) {
+router.get('/robots.txt', function (req, res) {
     res.type('text/plain');
     //res.render('robots', {layout: 'robots.txt'})
     res.send("Sitemap: https://www.telos-technology.com/sitemap.xml" +
       "   User-agent: *" +
        "   Allow: /");
-});*/
+});
 
 router.use(function (req, res, next) {
   if (!req.user) {
